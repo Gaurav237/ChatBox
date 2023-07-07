6e921889e4723f0d02ff862ef6fe028a7a336d24
Gaurav237 <gktiwary237@gmail.com>
2023-07-07 06:49:00 +0530

    installed express.js and dotenv

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const chats = require('./data/data');
+
+const dotenv = require('dotenv')
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is running");
+});
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params.id);
+    const singleChat = chats.find(c => c._id == req.params.id);
+    res.send(singleChat);
+});
+
+app.listen(port, (err) => {
+    if(err){
+        console.log('error in running the server ', err);
+        return;
+    }
+
+    console.log('Server is up and running on port', port);
+})
