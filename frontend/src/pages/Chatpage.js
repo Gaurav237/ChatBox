743c771ddd50a12f9912a9d57f2d7acd7a673ee7
Gaurav237 <gktiwary237@gmail.com>
2023-07-07 08:21:04 +0530

    fetched data from backend and displayed on Chatpage frontend

--- frontend/src/pages/Chatpage.js
+++ frontend/src/pages/Chatpage.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Chatpage = () => {
-  return (
-    <div>
-      Chatpage
-    </div>
-  )
+    // setChats is for setting chats value in State
+    // chats is used for fetching chats data
+    const [chats, setChats] = useState([]);
+
+    const fetchChats = async () => {
+        // axios used for makin asynchronous http requests
+        const {data} = await axios.get('/api/chat');
+        setChats(data);
+    }
+
+    // used hook for calling fetchChats after 
+    // this Chatpage component is rendered
+    useEffect(() => {
+        fetchChats();
+    }, []);
+
+    return (
+        <div>
+          { chats.map((chat) => {
+            return (
+            <div key={chat._id}> { chat.chatName } </div>
+          )})}
+        </div>
+    )
 }
 
 export default Chatpage;
