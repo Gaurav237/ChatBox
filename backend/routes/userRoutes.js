fd678caef8251e230077c4d28b1f1d8f3c3b1998
Gaurav237 <gktiwary237@gmail.com>
2023-07-10 11:14:11 +0530

    authorization middleware added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { registerUser, authenticateUser, allUsers } = require('../controllers/userControllers');
+const protect = require('../middleware/authorizeMiddleware');
 
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/login', authenticateUser);
-router.get('/', allUsers);
+router.get('/', protect, allUsers);
 
 module.exports = router;
