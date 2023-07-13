dc1330583d3c9924b00f02d76071ba132a8af06e
Gaurav237 <gktiwary237@gmail.com>
2023-07-13 13:34:07 +0530

    MyChat Structure added

--- frontend/src/components/miscellaneous/MyChats.js
+++ frontend/src/components/miscellaneous/MyChats.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { ChatState } from '../../context/ChatProvider';
-import { useToast } from '@chakra-ui/react';
+import { Box, Button, Stack, useToast, Text } from '@chakra-ui/react';
 import axios from 'axios';
+import { AddIcon } from '@chakra-ui/icons';
+import ChatLoading from '../others/ChatLoading';
+import { getSender } from '../../config/ChatMethods';
 
 const MyChats = () => {
-  const [loggedUser, setLoggedUser] = useState();
+  const [loggedUser, setLoggedUser] = useState('');
   const {user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
   const toast = useToast();
 
@@ -37,9 +40,78 @@ const MyChats = () => {
   }, []);
 
   return (
-    <div>
-      My Chats
-    </div>
+    <Box
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDirection="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="xl"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        fontFamily="Work sans"
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Chats
+          <Button
+            display="flex"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+      </Box>
+
+      <Box
+        display="flex"
+        flexDirection="column"
+        p={3}
+        bg="#F8F8F8"
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+      {
+        chats ?
+        (
+          <Stack overflowY='scroll'>
+            {chats.map((chat) => {
+              return <Box 
+                onClick={() => setSelectedChat(chat)}
+                cursor='pointer'
+                bg={selectedChat === chat ? '#38B2AC' : '#E8E8E8'}
+                color={selectedChat === chat ? "white" : "black"}
+                px={3} py={2}
+                borderRadius='lg'
+                key={chat._id}
+              >
+                <Text>
+                  {
+                  chat.isGroupChat 
+                  ? (chat.chatName) 
+                  : getSender(loggedUser, chat.users) 
+                  }
+                </Text>
+              </Box>
+            })}
+          </Stack>
+        ) 
+        : (
+          <ChatLoading />
+        )
+        
+      }
+      </Box>
+    </Box>
   )
 }
 
---
+++ frontend/src/config/ChatMethods.js
@@ -0,0 +1,5 @@
+
+// show the chat name as user which is not logged in for 1-on-1 chat
+export const getSender = (loggedUser, users) => {
+    return (users[0]._id === loggedUser._id) ? users[1].name : users[0].name;
+} 
